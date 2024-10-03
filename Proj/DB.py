ce738385d5567be7e5d8671a2292360a45b46ae5
Karim <chagarovkk@gmail.com>
2024-10-03 14:38:19 +0400

    связал БД

---
+++ Proj/DB.py
@@ -0,0 +1,24 @@
+from psycopg2 import OperationalError, connect
+
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+connection = create_connection(
+    "learn_market", "postgres", "12qwaszx", "localhost", "5432"
+)
+
+
